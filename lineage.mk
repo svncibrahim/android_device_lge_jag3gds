65956b350c80d8499a36293fc68771ebd5350498
svncibrahim <ibrahim.sevinc.mail@gmail.com>
2017-01-07 19:28:34 +0200

    Update and rename cm.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,4 +1,4 @@
-# Inherit some common CM stuff.
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/jag3gds/device_jag3gds.mk)
@@ -9,7 +9,7 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Release name
 PRODUCT_RELEASE_NAME := LG G3s
-PRODUCT_NAME := cm_jag3gds
+PRODUCT_NAME := lineage_jag3gds
 PRODUCT_MODEL := LG-D724
 PRODUCT_DEVICE := jag3gds
 PRODUCT_MANUFACTURER := LGE
